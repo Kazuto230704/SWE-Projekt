fcdaa401b7218a54ffc2b33b641df1f47921c073
Kazuto230704 <andrea.knobloch8@gmail.com>
2024-06-26 10:40:12 +0200

    update

---
+++ Taskmanager/Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Taskmanager
+{
+    public class Task
+    {
+        private int id;
+        private string discription;
+        private bool completed;
+
+        public int Id { set { id = value; } get { return id; } }
+        public string Discription { set; get; }
+        public bool Completed { set; get; }
+
+        Random rnd = new Random();
+        int num = rnd.NextInt(0, 100);
+
+        public Task(string discription)
+        {
+
+            Id = num;
+            Discription = discription;
+            Completed = false;
+        }
+    }
+}
---
+++ Taskmanager/Taskmanager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Taskmanager
+{
+    public class Taskmanager
+    {
+        private Task[] liste;
+        private int Count = 0;
+
+        public Taskmanager()
+        {
+
+        }
+
+        public Task dd
+        {
+            get => default(Taskmanager.Task);
+            set
+            {
+            }
+        }
+
+        public void addTask(string discription)
+        {
+            liste[Count] = new Task(discription);
+            Count++;
+        }
+
+        public void showTasks()
+        {
+            foreach (Task task in liste)
+            {
+                System.Console.WriteLine(task.ToString());
+            }
+
+        }
+
+        public void markTaskAsCompleted(int num)
+        {
+            liste[num].Completed = true;
+        }
+
+        public void showCompletedTasks()
+        {
+            foreach (Task task in liste)
+            {
+                if (task.Completed == true)
+                {
+                    System.Console.WriteLine(task.ToString());
+                }
+            }
+        }
+    }
+}
